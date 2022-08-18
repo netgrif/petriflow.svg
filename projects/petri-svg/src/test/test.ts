d1473954402cbf9157518ff12f6b3c8b5889291d
Milan Mladoniczky <6153201+mladoniczky@users.noreply.github.com>
2022-08-18 18:04:24 +0200

    Fix petri.svg test

--- projects/petri-svg/src/test/test.ts
+++ projects/petri-svg/src/test/test.ts
@@ -1,12 +1,10 @@
-import {test} from "@jest/globals";
-
-
 describe('Petri Nets SVG tests', () => {
 
     beforeEach(() => {
         console.log("Before each test this will be run")
     });
 
+    // @ts-ignore
     test('should start', () => {
         expect(1).toEqual(1);
     });
